b95d4308979bc1419f453bcaaf4dfdadfddeb726
Leon Sam <18523441+Leon-Sam@users.noreply.github.com>
2023-08-20 11:21:52 -0400

    Delete extranous files

---
+++ components/testchart.jsx
@@ -0,0 +1,16 @@
+
+
+export default () => (
+    
+    <>
+    <p className="bg-red-950">
+        Hello, <span className="text-blue-500">world</span>
+    </p>
+    
+    <div className="w-40 h-40 bg-red-500 text-white flex justify-center items-center">
+      Red Box
+    </div>
+
+    </>
+
+);
